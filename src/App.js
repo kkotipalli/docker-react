d2eeb69c24561edd6d073b26758a17f3d4fa7423
kkotipalli <karthikkoti@gmail.com>
2020-03-28 21:11:23 +0530

    changed code

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-            Learn React
+            Learn React! React is good!
         </a>
       </header>
     </div>
